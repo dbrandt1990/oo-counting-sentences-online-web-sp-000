abe2f6bcf17db13486627da9cda8fa76cba37a7e
Danny Brandt <dvbrandt90@gmail.com>
2021-01-02 16:41:37 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+   self.end_with?("?")
   end
 
   def exclamation?
-
+   self.end_with?("!")
   end
 
   def count_sentences
-
+    
+    count = 0
+    if(!self.include?("."||"?"||"!"))
+      return 0
+    else
+      puts self.split(/[.?!]/
+      temp = self.split(/[.?!]/).count
+      binding.pry
+    end
   end
 end
